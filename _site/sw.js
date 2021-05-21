0345e7f8d5c006361cba7e98ecd2c9d0220a052d
Yongxing Zeng <yongxingzeng@iMac.local>
2021-05-22 00:16:29 +0800

    add new post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210522.0010';
+const cacheName = 'chirpy-20210522.0015';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
